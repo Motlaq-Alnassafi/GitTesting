6dd79c9fc8149425acd379ec0e253f15553a9d56
Motlaq <motlaqalnesafi@gmail.com>
2023-09-25 15:29:08 +0300

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Eng. Motlaq Alnassafi on 25/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
